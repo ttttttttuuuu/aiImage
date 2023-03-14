1cbe731feb43a65d38a85ee0a1191e4cd5227924
tt <sydney118227@icloud.com>
2023-03-14 23:40:56 +0800

    3.0

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,14 @@ import dalleRoutes from "./routes/dalleRoutes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
